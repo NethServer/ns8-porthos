51b6ef966e8e6a1b8f3ab5201526d1d25dffd929
Davide Principi <davide.principi@nethesis.it>
2024-01-30 17:17:13 +0100

    Implement legacy tier policy
    Three tiers, with a weighted distribution of systems (10%, 20%, 70%)
    based on a systemd_id hashing function.
    
    Systems must provide their credentials with HTTP Basic authentication.
    
    Serve the /distfeed/repodata.json file.

--- imageroot/script/snapshot.php
+++ imageroot/script/snapshot.php
@@ -7,18 +7,73 @@
 
 ini_set('date.timezone', 'UTC');
 
-$uri = $_SERVER['DOCUMENT_URI']; // XXX parse URI
-
 // Disable the Content-Type header in PHP, so that nginx x-accel can add its own
 ini_set('default_mimetype', FALSE);
 
-$snapshots = array_map('basename', glob('/srv/porthos/webroot/d20*'));
-
-if (count($snapshots) > 0) {
-    sort($snapshots);
-    header('Cache-Control: private');
-    header('X-Accel-Redirect: /' . end($snapshots) . $uri);
-} else {
-    http_response_code(404);
-    echo "Not found\n";
+// Return the 0-based tier id for the given $system_id
+function system_tier($system_id) {
+    $idx = abs(crc32($system_id)) % 100;
+    if($idx < 10) {
+        $tier_id = 0;
+    } elseif($idx < 30) {
+        $tier_id = 1;
+    } else {
+        $tier_id = 2;
+    }
+    return $tier_id;
 }
+
+// Return the age (days) of the given $snapshot
+function snapshot_age($snapshot) {
+    $year = substr($snapshot, 1, 4);
+    $month = substr($snapshot, 5, 2);
+    $day = substr($snapshot, 7, 2);
+    $hour = substr($snapshot, 10, 2);
+    $minute = substr($snapshot, 12, 2);
+    $second = substr($snapshot, 14, 2);
+    $frac = substr($snapshot, 16, 2);
+
+    $dt_current = new DateTime();
+    $dt_snapshot = new DateTime("$year-$month-$day $hour:$minute:$second.$frac");
+
+    // Calculate the difference in days between the two dates
+    $dt_interval = $dt_current->diff($dt_snapshot);
+
+    $days_age = $dt_interval->format('%a');
+    return $days_age;
+}
+
+function main() {
+    // Minimum age (days) expected by tiers
+    $tier_age = [3, 4, 5];
+
+    $lsnapshots = array_map('basename', glob('/srv/porthos/webroot/d20*'));
+
+    if (count($lsnapshots) == 0) {
+        http_response_code(404);
+        echo "Not found\n";
+    }
+
+    $username = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : "";
+    $password = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : "";
+
+    sort($lsnapshots);
+
+    $snapshot = array_pop($lsnapshots);
+    if($username && $password) {
+        // Authenticated clients gain access to older tiers
+        while($snapshot != NULL) {
+            $tier_id = system_tier($username);
+            if (snapshot_age($snapshot) > $tier_age[$tier_id]) {
+                break;
+            }
+            $snapshot = array_pop($lsnapshots);
+        }
+    }
+
+    header('Cache-Control: private');
+    header('X-Accel-Redirect: /' . $snapshot . $_SERVER['DOCUMENT_URI']);
+}
+
+// Run
+main();
